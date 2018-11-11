500711558a2db045902469eac1e417a7fe1072d5
ChenHong <2675601413@qq.com>
2018-11-11 11:05:04 +0800

    commit

--- 历年来面试总结/2018面试总结笔记/今日头条社招面试题/编程题/最少硬币找零问题-动态规划.js
+++ 历年来面试总结/2018面试总结笔记/今日头条社招面试题/编程题/最少硬币找零问题-动态规划.js
@@ -11,26 +11,27 @@
 
 function MinCoinChange(coins){
     this.coins = coins;
-    var cache = {};
+    var cache = {};  //存所有情况的最小硬币找零问题
 
     this.makeChange = function(amount){
         var me = this;
-        if(!amount){
+        if(!amount){  //传入的钱数为0，返回空
             return [];
         }
 
-        if(cache[amount]){
+        if(cache[amount]){   //传入的钱数，已经计算过了最少硬币数，因为存在cache里了，直接返回
             return cache[amount];
         }
 
-        var min = [], newMin, newAmount;
+        var min = [], newMin, newAmount;  //最少硬币的数组，剩余钱数的最少硬币数组，剩余的要找的钱数
         for(var i=0; i<coins.length; i++){
             var coin = coins[i];
             newAmount = amount - coin;
 
             if(newAmount >= 0){
-                newMin = me.makeChange(newAmount);
+                newMin = me.makeChange(newAmount);  //剩余钱数的最少硬币数
             }
+
             if(
                 newAmount >= 0 &&
                 (newMin.length < min.length-1 || !min.length)
@@ -39,6 +40,10 @@ function MinCoinChange(coins){
                 min = [coin].concat(newMin);
             }
         }
+        console.log(cache);
         return (cache[amount] = min);
     };
 }
+
+var minCoinChange = new MinCoinChange([1, 5, 10, 25]);
+console.log(minCoinChange.makeChange(36));
